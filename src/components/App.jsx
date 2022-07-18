56ee6ea851cebd856a34caee3faeda415e2d5fbe
Natalia Monasturska <tjomamon@gmail.com>
2022-07-18 22:57:58 +0300

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,42 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from "react";
+import {Statistics } from 'components/Statistics/Statistics';
+
+
+
+
+export class App extends Component {
+  state = {
+    good: 2,
+    neutral: 4,
+    bad: 4
+  }
+
+  countTotalFeedback = () => {
+    const {good, neutral, bad} = this.state;
+    const total = good + neutral + bad;
+    return total;
+  }
+
+  countPositiveFeedbackPercentage = () => {
+    const {good, neutral, bad} = this.state;
+    const persentage =  Math.round(good/(good + neutral + bad)*100);
+    return persentage;                                
+  }
+
+  render() {
+    return <div>
+      <Statistics 
+      good={this.state.good} 
+      neutral={this.state.neutral} bad={this.state.bad} 
+      total={this.countTotalFeedback()} 
+      positivePercentage={this.countPositiveFeedbackPercentage()}/>
+
+      <FeedbackOptions options={} onLeaveFeedback={}/>
+    </div>;
+  }
+}
+
+
+
+
+
